73337eb2aad7428e53cf7f55df5b8c9819b1d155
Geomovil <gonzalo.murgueytio@geomovil.net>
2018-01-17 15:03:51 -0500

    Fin de la sección 16

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -10,6 +10,9 @@ import { RxjsComponent } from './rxjs/rxjs.component';
 import { LoginGuardGuard } from '../services/service.index';
 import { ProfileComponent } from './profile/profile.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
+import { HospitalesComponent } from './hospitales/hospitales.component';
+import { MedicosComponent } from './medicos/medicos.component';
+import { MedicoComponent } from './medicos/medico.component';
 
 
 const pagesRoute: Routes = [
@@ -27,6 +30,9 @@ const pagesRoute: Routes = [
       // Mantenimientos
       { path: 'perfil', component: ProfileComponent, data: { titulo: 'Perfil de Usuario' }},
       { path: 'usuarios', component: UsuariosComponent, data: { titulo: 'Mantenimiento de Usuarios' }},
+      { path: 'hospitales', component: HospitalesComponent, data: { titulo: 'Mantenimiento de Hospitales' }},
+      { path: 'medicos', component: MedicosComponent, data: { titulo: 'Mantenimiento de Médicos' }},
+      { path: 'medico/:id', component: MedicoComponent, data: { titulo: 'Actualizar Médicos' }},
       { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     ]
   }
--- src/app/services/service.module.ts
+++ src/app/services/service.module.ts
@@ -2,6 +2,7 @@ import { ModalUploadService } from './../components/modal-upload/modal-upload.se
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsService, SidebarService, SharedService, UsuarioService, LoginGuardGuard, SubirArchivoService } from './service.index';
+import { HospitalService, MedicoService } from './service.index';
 import { HttpClientModule } from '@angular/common/http';
 
 
@@ -17,7 +18,9 @@ import { HttpClientModule } from '@angular/common/http';
     UsuarioService,
     LoginGuardGuard,
     SubirArchivoService,
-    ModalUploadService
+    ModalUploadService,
+    HospitalService,
+    MedicoService
   ],
   declarations: []
 })
