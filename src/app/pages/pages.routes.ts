a6cd7a3c126cadbbd5af0749bc3097633d7492b6
Geomovil <gonzalo.murgueytio@geomovil.net>
2018-01-18 15:06:10 -0500

    Fin secciòn 17

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -7,12 +7,15 @@ import { PagesComponent } from './pages.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
-import { LoginGuardGuard } from '../services/service.index';
 import { ProfileComponent } from './profile/profile.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 import { HospitalesComponent } from './hospitales/hospitales.component';
 import { MedicosComponent } from './medicos/medicos.component';
 import { MedicoComponent } from './medicos/medico.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+// Guards
+import { LoginGuardGuard } from '../services/service.index';
+import { AdminGuard } from '../services/service.index';
 
 
 const pagesRoute: Routes = [
@@ -27,9 +30,18 @@ const pagesRoute: Routes = [
       { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' }},
       { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' }},
       { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajustes de Tema' }},
-      // Mantenimientos
       { path: 'perfil', component: ProfileComponent, data: { titulo: 'Perfil de Usuario' }},
-      { path: 'usuarios', component: UsuariosComponent, data: { titulo: 'Mantenimiento de Usuarios' }},
+      { path: 'busqueda/:termino', component: BusquedaComponent, data: { titulo: 'Buscador' }},
+      // Mantenimientos
+      {
+        path: 'usuarios',
+        component: UsuariosComponent,
+        canActivate: [AdminGuard],
+        data: { titulo: 'Mantenimiento de Usuarios' }
+      },
+
+
+
       { path: 'hospitales', component: HospitalesComponent, data: { titulo: 'Mantenimiento de Hospitales' }},
       { path: 'medicos', component: MedicosComponent, data: { titulo: 'Mantenimiento de Médicos' }},
       { path: 'medico/:id', component: MedicoComponent, data: { titulo: 'Actualizar Médicos' }},
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -3,8 +3,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Usuario } from '../../models/usuario.model';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 import { Router } from '@angular/router';
 import { SubirArchivoService } from '../subir-archivo/subir-archivo.service';
+import { Observable } from 'rxjs/Observable';
 
 declare var swal: any;
 
@@ -13,6 +15,7 @@ export class UsuarioService {
 
   usuario: Usuario;
   token: string;
+  menu: any = [];
 
   constructor(
     public http: HttpClient,
@@ -30,25 +33,31 @@ export class UsuarioService {
     if (localStorage.getItem('token')) {
       this.token = localStorage.getItem('token');
       this.usuario = JSON.parse(localStorage.getItem('usuario'));
+      this.menu = JSON.parse(localStorage.getItem('menu'));
     }else {
       this.token = '';
       this.usuario = null;
+      this.menu = [];
     }
   }
 
-  guardarStorage(id: string, token: string, usuario: Usuario) {
+  guardarStorage(id: string, token: string, usuario: Usuario, menu: any) {
     localStorage.setItem('id', id);
     localStorage.setItem('token', token);
     localStorage.setItem('usuario', JSON.stringify(usuario));
+    localStorage.setItem('menu', JSON.stringify(menu));
     this.usuario = usuario;
     this.token = token;
+    this.menu = menu;
   }
 
   logout() {
     this.usuario = null;
     this.token = '';
+    this.menu = [];
     localStorage.removeItem('token');
     localStorage.removeItem('usuario');
+    localStorage.removeItem('menu');
     this.router.navigate(['/login']);
   }
 
@@ -56,7 +65,7 @@ export class UsuarioService {
     let url = URL_SERVICIOS + '/login/google';
     return this.http.post(url, {token})
           .map( (resp: any) => {
-           this.guardarStorage(resp.id, resp.token, resp.usuario);
+           this.guardarStorage(resp.id, resp.token, resp.usuario, resp.menu);
             return true;
           });
   }
@@ -70,8 +79,11 @@ export class UsuarioService {
     let url = URL_SERVICIOS + '/login';
     return this.http.post(url, usuario)
           .map( (resp: any) => {
-            this.guardarStorage(resp.id, resp.token, resp.usuario);
+            this.guardarStorage(resp.id, resp.token, resp.usuario, resp.menu);
             return true;
+          }).catch(err => {
+            swal('Error en el login', err.error.mensaje, 'error');
+            return Observable.throw(err);
           });
   }
 
@@ -81,6 +93,9 @@ export class UsuarioService {
             .map( (resp: any) => {
               swal('Usuario creado', usuario.email, 'success');
               return resp.usuario;
+            }).catch(err => {
+              swal(err.error.mensaje, err.error.errors.message, 'error');
+              return Observable.throw(err);
             });
   }
 
@@ -91,10 +106,13 @@ export class UsuarioService {
             .map( (resp: any) => {
               if (usuario._id === this.usuario._id) {
                 let usuarioDB: Usuario = resp.usuario;
-                this.guardarStorage(usuarioDB._id, this.token, usuarioDB);
+                this.guardarStorage(usuarioDB._id, this.token, usuarioDB, this.menu);
               }
               swal('Usuario Actualizado', usuario.nombre, 'success');
               return true;
+            }).catch(err => {
+              swal(err.error.mensaje, err.error.errors.message, 'error');
+              return Observable.throw(err);
             });
 
   }
@@ -104,7 +122,7 @@ export class UsuarioService {
           .then( (resp: any) => {
             this.usuario.img = resp.usuario.img;
             swal('Imagen Actualizada', this.usuario.nombre, 'success');
-            this.guardarStorage(id, this.token, this.usuario);
+            this.guardarStorage(id, this.token, this.usuario, this.menu);
           }).catch( resp => {
             console.error(resp);
           });
